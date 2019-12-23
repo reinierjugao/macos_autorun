e77ab4c654bcd4fee133d2f917a10fb3d839355f
Reinier Jugao <reinierjugao@gmail.com>
2019-12-22 19:53:44 -0800

    Removed raw applescript file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPTNAME="add_new_folder_autorun_script.scpt"
+SCRIPTNAME="add_new_folder_autorun_script.applescript"
 
 sudo cp $SCRIPTNAME /Library/Scripts/Folder\ Action\ Scripts/$SCRIPTNAME
 
